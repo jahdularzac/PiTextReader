7723d01008182dbcf82f438e17737b30ffc2b482
jean alvin <61833612+mediaenlab@users.noreply.github.com>
2021-03-18 15:19:40 +0100

    Add files via upload

---
+++ txtpico.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#tr "\n" " " < /tmp/text.txt > /tmp/text1line.txt
+#paste -s -d ' ' /tmp/text.txt
+
+tr "\n" " " < /tmp/text.txt > /tmp/text1line.txt
+pico2wave -l fr-FR -w /tmp/text.wav < /tmp/text1line.txt
+aplay /tmp/text.wav
